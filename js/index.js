4b1df0a68e51b519184bf08a9ddf8f13f053df93
D3monDLord <168437652+D3monDLord@users.noreply.github.com>
2024-05-26 11:45:13 +0545

    hi

--- js/index.js
+++ js/index.js
@@ -1,9 +1,33 @@
-function oi() {
-    let a = 1;
+function addition() {
+    let a = document.getElementById("num_one").value;
 
-    let b = 69;
+    let b = document.getElementById("num_two").value;
 
-    console.log(a * b);
+    let c = parseInt(a) + parseInt(b)
 
+    document.getElementById("result").innerHTML = c;
 }
-alert(oi);
+
+
+// let temperature = prompt("Enter temperature");
+
+// if (temperture != 10) {
+//     console.log("cold");
+// } else {
+//     console.log("hot");
+// }
+
+
+let temperature = prompt("Enter temperture");
+
+if(tempera ture<10){
+    console.log("cold")
+}
+
+else if(temperature >10 && temperature <30 ){
+    console.log("normal")
+}
+
+else{
+    console.log("hot")
+}
