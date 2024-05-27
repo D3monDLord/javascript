cea03e07dd2b42bda9248586245d3a02b61e29a9
D3monDLord <168437652+D3monDLord@users.noreply.github.com>
2024-05-27 12:00:06 +0545

    Add files via upload

---
+++ certificate/ceritifcate/index.js
@@ -0,0 +1,11 @@
+function certificate() {
+    let name = document.getElementById("n").value;
+
+    document.getElementById("name").innerHTML = name;
+
+    let idk = document.getElementById("u").value;
+
+    document.getElementById("idk").innerHTML = idk;
+
+    document.getElementById("child").style.display = "flex";
+}
